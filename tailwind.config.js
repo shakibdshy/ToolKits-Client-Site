273f672c44a202af1e93cd990f5eb359a2625113
Shakib D shy <shakibdshy@gmail.com>
2022-05-25 13:17:40 +0600

    Header Section Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#7432ff",
+          "secondary": "#1d233a",
+          "accent": "#1d233a",
+          "neutral": "#c5f617",
+          "base-100": "#FFFFFF",
+          "info": "#4a4f61",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  
+  plugins: [
+    require('daisyui'),
+  ],
+} 

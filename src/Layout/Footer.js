00622c8dc98b13ca6465d08e4341ea3e1dcf0f5a
Shakib D shy <shakibdshy@gmail.com>
2022-05-25 15:03:42 +0600

    Footer update

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
                         <Link to='/' className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                             <img src={logo} alt="" />
                         </Link>
-                        <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2022 ToolKits —
+                        <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© {new Date().getFullYear()} ToolKits —
                             <Link to='/' href="https://github.com/" className="text-gray-600 ml-1" target="_blank">@shakibdshy</Link>
                         </p>
                         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">

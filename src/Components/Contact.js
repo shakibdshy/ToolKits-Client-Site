1fdbf8659ec8f40d3e66a0a128d4e5940280baab
Shakib D shy <shakibdshy@gmail.com>
2022-05-25 18:18:51 +0600

    Contact page Update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Contact = () => {
     return (
         <>
-            <section className="bg-[#fafafb] text-gray-600 body-font relative">
+            <section className="bg-gray-100 text-gray-600 body-font relative">
                 <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
                     <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
                         <iframe className="absolute inset-0" style={{ filter: "grayscale(1)" + "contrast(1.2)" + "opacity(0.4)"}} title="map" marginHeight="0" marginWidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" width="100%" height="100%" frameBorder="0"></iframe>

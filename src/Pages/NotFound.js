55a77171ecc047ffc161c970523b50621435ef3a
Shakib D shy <shakibdshy@gmail.com>
2022-05-25 19:37:17 +0600

    Not Found page Update

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            This is a 404 page
-        </div>
+        <section className='py-[150px] text-center'>
+            <h3 className='mb-0 text-[100px] font-light text-[#222423] tracking-[20px] uppercase leading-none'>Oops!</h3>
+            <h2 className='text-[170px] font-bold text-primary leading-[170px] uppercase'>404</h2>
+            <h4 className='mb-8 font-light text-[#222423] text-[42px] uppercase'>Page Not Found</h4>
+            <Link className='btn btn-primary' to='/'>Back To Home</Link>
+        </section>
     );
 };
 

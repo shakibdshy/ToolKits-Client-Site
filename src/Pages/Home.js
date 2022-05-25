6066b2016f03f6e980a2b90481275fadafc40316
Shakib D shy <shakibdshy@gmail.com>
2022-05-25 14:38:56 +0600

    Counter Section Update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Components/Banner';
+import Counter from '../Components/Counter';
 
 const Home = () => {
     return (
         <>
             <Banner />
+            <Counter />
         </>
     );
 };

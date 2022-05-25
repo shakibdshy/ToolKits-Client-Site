460b333c734fd2ecad64bb5269e0edbacb7e5f5e
Shakib D shy <shakibdshy@gmail.com>
2022-05-25 16:26:35 +0600

    Contact Page Update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Contact = () => {
     return (
         <>
-            <section className="text-gray-600 body-font relative">
+            <section className="bg-[#fafafb] text-gray-600 body-font relative">
                 <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
                     <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
                         <iframe className="absolute inset-0" style={{ filter: "grayscale(1)" + "contrast(1.2)" + "opacity(0.4)"}} title="map" marginHeight="0" marginWidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" width="100%" height="100%" frameBorder="0"></iframe>
@@ -21,7 +21,7 @@ const Contact = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 px-8 mt-8 md:mt-0">
                         <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
                         <div className="relative mb-4">
                             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
@@ -35,7 +35,7 @@ const Contact = () => {
                             <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
                             <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                         </div>
-                        <button className="btn btn-primary">Button</button>
+                        <button className="btn btn-primary">Submit</button>
                     </div>
                 </div>
             </section>   

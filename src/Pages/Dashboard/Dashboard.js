b19a175aec924efd890faa50d5552a94b5ec1fc8
Shakib D shy <shakibdshy@gmail.com>
2022-05-27 01:44:26 +0600

    My Order Page Ui Update

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link, NavLink, Outlet } from 'react-router-dom';
+import { BsCart3 } from 'react-icons/bs';
+import { MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiStarsStack } from 'react-icons/gi';
+import { FaCartArrowDown } from 'react-icons/fa';
+import { AiOutlineUserSwitch } from 'react-icons/ai';
+import { BiUser } from 'react-icons/bi';
 
 const Dashboard = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="drawer drawer-mobile bg-[#f8f9fa]">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="container mx-auto px-8 py-14 drawer-content flex flex-col">
+                    <h2 className='text-2xl font-bold mb-8'>My Dashboard</h2>
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    <Outlet />
+                </div>  
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu p-4 overflow-y-auto w-64 bg-base-100 text-base-content">
+                        <li><NavLink to='/dashboard'><span><BsCart3 /></span> My Order</NavLink></li>
+                        <li><NavLink to='/dashboard/all-order'><span><BsCart3 /></span> All Order</NavLink></li>
+                        <li><NavLink to='/dashboard/manage-products'><span><MdOutlineProductionQuantityLimits /></span> Manage All Products</NavLink></li>
+                        <li><NavLink to='/dashboard/review'><span><GiStarsStack /></span> Add A Review</NavLink></li>
+                        <li><NavLink to='/dashboard/add-product'><span><FaCartArrowDown /></span> Add A Product</NavLink></li>
+                        <li><NavLink to='/dashboard/make-admin'><span><AiOutlineUserSwitch /></span> Make Admin</NavLink></li>
+                        <li><NavLink to='/dashboard/my-profile'><span><BiUser /></span> My Profile</NavLink>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>   
+        </>
     );
 };
 
--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -2,9 +2,44 @@ import React from 'react';
 
 const MyOrder = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="dashboard-table-container overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Product Name</th>
+                            <th>Quantity</th>
+                            <th>Status</th>
+                            <th>Paid</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Jessica Moore</td>
+                            <td><span className='badge badge-warning'>Pending</span></td>
+                            <td><button className='btn btn-light-cancel' type="button">Cancel</button></td>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <td>Cy Ganderton</td>
+                            <td>Jessica Moore</td>
+                            <td><span className='badge badge-success'>Success</span></td>
+                            <td><button className='btn btn-success' type="button">Shipped</button></td>
+                        </tr>
+                        <tr>
+                            <th>3</th>
+                            <td>Cy Ganderton</td>
+                            <td>Jessica Moore</td>
+                            <td><span className='badge badge-error'>Cancel</span></td>
+                            <td><button className='btn btn-light-cancel' type="button">Cancel</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>   
+        </>
     );
 };
 

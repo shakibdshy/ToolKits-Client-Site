b78dad17edabda10946ba4d7973317422cecb674
Shakib D shy <shakibdshy@gmail.com>
2022-05-29 01:31:57 +0600

    Update Add Product

--- src/Pages/Dashboard/AddAProduct.js
+++ src/Pages/Dashboard/AddAProduct.js
@@ -1,18 +1,82 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import auth from '../../utils/firebase.init';
 
 const AddAProduct = () => {
+    const [user] = useAuthState(auth);
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    const imageStorageKey = '54aa09338987dbac2411ee1063e11ced';
+
+    const onSubmit = async data => {
+        console.log(data);
+        const image = data.img[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        toast.warn('Adding product. It may take a while');
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.success) {
+                    const img = result.data.url;
+                    const name = data.name;
+                    const content = data.content;
+                    const newAvailable = parseInt(data.available);
+                    const newMinQuantity = parseInt(data.minQuantity);
+                    const newPrice = parseInt(data.price);
+                    const product = {
+                        name: name,
+                        content: content,
+                        available: newAvailable,
+                        minQuantity: newMinQuantity,
+                        price: newPrice,
+                        img: img
+                    };
+                    console.log(product);
+                    //adding item to database
+                    fetch('http://localhost:5000/tools', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify(product)
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.success) {
+                                toast.success("Product added successfully !");
+                            }
+                        })
+                }
+            })
+        reset();
+    }
+
     return (
         <>
             <div className='review-section'>
                 <div className='container mx-auto px-14 max-w-4xl'>
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div className='dashboard-box-container flex items-center space-x-6'>
                             <div className="shrink-0">
                                 <img className="h-32 w-32 object-cover rounded-3xl" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80" alt="" />
                             </div>
                             <label className="block">
                                 <span className="sr-only">Choose profile photo</span>
-                                <input type="file" className="block w-full text-sm text-[#323232]
+                                <input type="file"
+                                    {...register("img", {
+                                        required: {
+                                            value: true,
+                                            message: 'Image is Required'
+                                        }
+                                    })}
+                                    className="block w-full text-sm text-[#323232]
                                 bg-[#f8f9fa] rounded-2xl border border-solid border-[#f8f9fa]
                                 file:mr-4 file:py-2 file:px-4
                                 file:rounded-full file:border-0
@@ -32,11 +96,57 @@ const AddAProduct = () => {
                                 </div>
                             </div>
                             <div className="grid grid-cols-1 gap-6">
-                                <input type="text" placeholder="Product Name" className="input input-bordered input-secondary w-full" />
-                                <textarea placeholder="Short description" className="textarea textarea-secondary"></textarea>
-                                <input type="number" placeholder="Minimum order quantity" className="input input-bordered input-secondary w-full" />
-                                <input type="number" placeholder="Available quantity" className="input input-bordered input-secondary w-full" />
-                                <input type="number" placeholder="Price" className="input input-bordered input-secondary w-full" />
+                                <input type="text" placeholder="Product Name" {...register("name", {
+                                    required: {
+                                        value: true,
+                                        message: 'Product Name is Required'
+                                    }
+                                })} className="input input-bordered input-secondary w-full" />
+                                <label className="label">
+                                    {errors.name?.type === 'required' && <span className="label-text-alt text-red-400 text-sm">{errors.name.message}</span>}
+                                </label>
+                                <textarea {...register("content", {
+                                    required: {
+                                        value: true,
+                                        message: 'Description is Required'
+                                    }
+                                })} placeholder="Short description" className="textarea textarea-secondary" />
+                                <label className="label">
+                                    {errors.content?.type === 'required' && <span className="label-text-alt text-red-400 text-sm">{errors.content.message}</span>}
+                                </label>
+                                <input type="number"
+                                    {...register("minQuantity", {
+                                        required: {
+                                            value: true,
+                                            message: 'Minimum Quantity is Required'
+                                        }
+                                    })}
+                                    placeholder="Minimum order quantity" className="input input-bordered input-secondary w-full" />
+                                <label className="label">
+                                    {errors.minQuantity?.type === 'required' && <span className="label-text-alt text-red-400 text-sm">{errors.minQuantity.message}</span>}
+                                </label>
+                                <input type="number"
+                                    {...register("available", {
+                                        required: {
+                                            value: true,
+                                            message: 'Available Quantity is Required'
+                                        }
+                                    })}
+                                    placeholder="Available quantity" className="input input-bordered input-secondary w-full" />
+                                <label className="label">
+                                    {errors.available?.type === 'required' && <span className="label-text-alt text-red-400 text-sm">{errors.available.message}</span>}
+                                </label>
+                                <input type="number"
+                                    {...register("price", {
+                                        required: {
+                                            value: true,
+                                            message: 'Price is Required'
+                                        }
+                                    })}
+                                    placeholder="Price" className="input input-bordered input-secondary w-full" />
+                                <label className="label">
+                                    {errors.price?.type === 'required' && <span className="label-text-alt text-red-400 text-sm">{errors.price.message}</span>}
+                                </label>
                             </div>
                         </div>
                         <div className='dashboard-box-container'>
@@ -49,11 +159,13 @@ const AddAProduct = () => {
                                 </div>
                             </div>
                             <div className="grid grid-cols-1 gap-6">
-                                <input type="email" placeholder="Email" className="input input-bordered input-secondary w-full" />
+                                <input type="email"
+                                    {...register("email")}
+                                    placeholder="Email" value={user?.email} readOnly className="input input-bordered input-secondary w-full" />
                             </div>
                         </div>
                         <div className='dashboard-box-container flex items-center justify-between gap-1'>
-                            <button className='btn btn-light-secondary w-full' type="button">Add</button>
+                            <button className='btn btn-light-secondary w-full' type="submit">Add</button>
                         </div>
                     </form>
                 </div>

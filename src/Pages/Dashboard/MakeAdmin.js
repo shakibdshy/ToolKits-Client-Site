ae610a8ca7e434abb12bd0bd6d9a6222de0ce14b
Shakib D shy <shakibdshy@gmail.com>
2022-05-29 20:39:46 +0600

    MakeAdmin Update

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -4,67 +4,70 @@ import { toast } from 'react-toastify';
 import HashLoader from "react-spinners/HashLoader";
 
 const MakeAdmin = () => {
-    // const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/all-users', {
-    //     method: 'GET',
-    //     headers: {
-    //         authorization: `Bearer ${localStorage.getItem('accessToken')}`
-    //     }
-    // }).then(res => res.json()));
-    // console.log(users);
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/all-users', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    console.log(users);
 
-    // if (isLoading) {
-    //     return <HashLoader className='mx-auto mt-4' color={"#36D7B7"} size={30} />;
-    // }
+    if (isLoading) {
+        return <HashLoader className='mx-auto mt-4' color={"#36D7B7"} size={30} />;
+    }
 
-    // const makeAdmin = () => {
-    //     console.log('making admin');
-    //     // const email = users.email;
-    //     fetch(`http://localhost:5000/user/admin/${users.email}`, {
-    //         method: 'PUT',
-    //         headers: {
-    //             authorization: `Bearer ${localStorage.getItem('accessToken')}`
-    //         }
-    //     })
-    //         .then(res => {
-    //             if (res.status === 403) {
-    //                 toast.error('Failed to Make an admin');
-    //             }
-    //             return res.json()
-    //         })
-    //         .then(data => {
-    //             if (data.modifiedCount > 0) {
-    //                 refetch();
-    //                 toast.success(`Successfully made an admin`);
-    //             }
-    //         })
-    // }
+    const makeAdmin = (email) => {
+        console.log('making admin', email);
+
+        fetch(`http://localhost:5000/user-update/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Failed to Make an admin');
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.success) {
+                    toast.success(`Successfully made an admin`);
+                    refetch();
+                }
+            })
+    }
     return (
         <>
-            {/* <div className="dashboard-table-container overflow-x-auto">
+            <div className="dashboard-table-container overflow-x-auto">
                 <table className="table table-zebra w-full">
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>User Image</th>
-                            <th>User Name</th>
                             <th>User Email</th>
                             <th>Role</th>
                             <th>Remove</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th>1</th>
-                            <td>Image</td>
-                            <td>Name</td>
-                            <td>Email</td>
-                            <td className='flex items-center gap-2'>
-                                <button className='btn btn-light-cancel btn-sm' type="button">Remove User</button>
-                            </td>
-                        </tr>
+                        {
+                            users.map((user, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{ index + 1 }</td>
+                                        <td>{ user.email }</td>
+                                        <td>{user.role !== 'admin' ? <button onClick={() => makeAdmin(user.email)} className="btn btn-secondary btn-sm">Make Admin</button> : <p className='text-green-600'>Already an admin</p>}</td>
+                                        <td>
+                                            <button className='btn btn-light-cancel btn-sm' type="button">Remove User</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </table>
-            </div> */}
+            </div>
         </>
     );
 };

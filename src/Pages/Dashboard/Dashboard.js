d0f34436e6949c2cca5658abc94ffd5687d27ee3
Shakib D shy <shakibdshy@gmail.com>
2022-05-29 18:22:01 +0600

    Update make admin page

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,8 +6,13 @@ import { GiStarsStack } from 'react-icons/gi';
 import { FaCartArrowDown } from 'react-icons/fa';
 import { AiOutlineUserSwitch } from 'react-icons/ai';
 import { BiUser } from 'react-icons/bi';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../utils/firebase.init';
+import useAdmin from '../../Hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
         <>
             <div className="drawer drawer-mobile bg-[#f8f9fa]">
@@ -15,22 +20,21 @@ const Dashboard = () => {
                 <div className="container mx-auto px-8 py-14 drawer-content flex flex-col">
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
                     <Outlet />
-                </div>  
+                </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-64 bg-base-100 text-base-content">
+                        <li><NavLink to='/dashboard/my-profile'><span><BiUser /></span> My Profile</NavLink></li>
                         <li><NavLink to='/dashboard'><span><BsCart3 /></span> My Order</NavLink></li>
+                        <li><NavLink to='/dashboard/review'><span><GiStarsStack /></span> Add A Review</NavLink></li>
                         <li><NavLink to='/dashboard/all-order'><span><BsCart3 /></span> All Order</NavLink></li>
                         <li><NavLink to='/dashboard/manage-products'><span><MdOutlineProductionQuantityLimits /></span> Manage All Products</NavLink></li>
-                        <li><NavLink to='/dashboard/review'><span><GiStarsStack /></span> Add A Review</NavLink></li>
                         <li><NavLink to='/dashboard/add-product'><span><FaCartArrowDown /></span> Add A Product</NavLink></li>
                         <li><NavLink to='/dashboard/make-admin'><span><AiOutlineUserSwitch /></span> Make Admin</NavLink></li>
-                        <li><NavLink to='/dashboard/my-profile'><span><BiUser /></span> My Profile</NavLink>
-                        </li>
                     </ul>
 
                 </div>
-            </div>   
+            </div>
         </>
     );
 };
--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,10 +1,71 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
+import HashLoader from "react-spinners/HashLoader";
 
 const MakeAdmin = () => {
+    // const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/all-users', {
+    //     method: 'GET',
+    //     headers: {
+    //         authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    //     }
+    // }).then(res => res.json()));
+    // console.log(users);
+
+    // if (isLoading) {
+    //     return <HashLoader className='mx-auto mt-4' color={"#36D7B7"} size={30} />;
+    // }
+
+    // const makeAdmin = () => {
+    //     console.log('making admin');
+    //     // const email = users.email;
+    //     fetch(`http://localhost:5000/user/admin/${users.email}`, {
+    //         method: 'PUT',
+    //         headers: {
+    //             authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    //         }
+    //     })
+    //         .then(res => {
+    //             if (res.status === 403) {
+    //                 toast.error('Failed to Make an admin');
+    //             }
+    //             return res.json()
+    //         })
+    //         .then(data => {
+    //             if (data.modifiedCount > 0) {
+    //                 refetch();
+    //                 toast.success(`Successfully made an admin`);
+    //             }
+    //         })
+    // }
     return (
-        <div>
-            
-        </div>
+        <>
+            {/* <div className="dashboard-table-container overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>User Image</th>
+                            <th>User Name</th>
+                            <th>User Email</th>
+                            <th>Role</th>
+                            <th>Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Image</td>
+                            <td>Name</td>
+                            <td>Email</td>
+                            <td className='flex items-center gap-2'>
+                                <button className='btn btn-light-cancel btn-sm' type="button">Remove User</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div> */}
+        </>
     );
 };
 
